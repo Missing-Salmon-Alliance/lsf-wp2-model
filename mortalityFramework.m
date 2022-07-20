83e8fb27b0fbc5557ca4fc20d9e71560c9d00adc
neil <neil.banas@strath.ac.uk>
2022-07-20 15:07:49 +0100

    rewriting for clarity. todo.txt added

--- mortalityFramework.m
+++ mortalityFramework.m
@@ -1,39 +1,43 @@
 function [res,p,monthly,daily] = mortalityFramework(varargin);
 
 % Salmon Mortality Framework Model
-% v0.5.1, July 2022
+% v0.5.2, July 2022
 % Neil Banas, Emma Tyldesley, Colin Bull
 
-% to do -----------------------------
-%
-% adult duration as a function of size, and affecting marine mortality
-%  for the Bush, 6 mo at sea = 0.95 marine mortality,
-%               18 mo at sea = 0.98 marine mortality 
-%
-% actual cycles of prey and temperature in FW, to enable
-%     T effects beyond egg
-%     season-specific management scenarios
-
-
-% -----------------------------------
 
 
 % --- stage structure ---
-stages = {'egg','juvSum1','juvLater','smolt','earlyPS','latePS',...
+stages = {'egg','fry','parr','smolt','earlyPS','latePS',...
 		  'adultOc','adultCoastal','adultRiver','nextGen'};
 stages_longnames = ...
-	{'egg','juvenile first summer','juvenile after first summer','smolt',...
+	{'egg','fry','parr','smolt',...
 	'early post-smolt','late post-smolt',...
 	'adult in ocean','adult on coast','adult in river','next generation'};
 for i=1:length(stages)
-	s.(stages{i}) = i; % for more readable code, s.egg = 1, s.juvSum1 = 2, ...
+	s.(stages{i}) = i; % for more readable code, s.egg = 1, s.fry = 2, ...
 end
 
 
 % --- model parameters ------------------------------------------------------------------
 clear p
 p.N_initial = 1e6;
-p.baselineDuration_months = [5 6 6 1 3 5 4 2 4];
+
+
+% --- life history schedule ---
+
+p.yearday_eggDeposition = datenum('1 Nov 0000') - 366;
+p.baselineDuration_egg = 5; % in months
+p.yearday_endOfFry = datenum('30 Sep 0000');
+p.baselineDuration_parr = 6;
+p.flexibleParrDuration = 1; % if 1, choose parr duration based on length at end
+							% of fry stage, as opposed to this being set by the
+							% user via parameter values
+p.baselineDuration_smolt = 1;
+p.baselineDuration_earlyPS = 3;
+p.baselineDuration_latePS = 5;
+p.baselineDuration_adultOc = 4;
+p.baselineDuration_adultCoastal = 2;
+p.baselineDuration_adultRiver = 4;
 
 % --- growth params ---
 
@@ -79,9 +83,9 @@ p.exp_sizeMort = -1.57; % beta from Ricker 1976 -> Mangel 1994 -> IBASAM
 p.Lcutoff_sizeMort = 60; % beyond this stops applying size-dependent mortality.
 						% makes very little difference whether this is 25 or 60
 
-p.maxParr = 50000;      % juv first summer carrying capacity
+p.maxParr = 50000;      % fry-stage carrying capacity
 p.fryRicker = 0.08;     % hatching to parr Ricker stock-recruit parameter
-p.maxSmolts = 27000;    % juv later carrying capacity
+p.maxSmolts = 27000;    % parr-stage carrying capacity
 p.parrSmoltBH = 0.5;    % parr to smolt Beverton Holt stock-recruit parameter
     % the Ricker and BH parameters above all directly from Salmonmodeller
     % no references given
@@ -95,10 +99,10 @@ p.mort_parr_annual = 0.2; % additional mortality if the parr take 18 mo instead 
 
 p.dTwinter = 0; % degrees relative to baseline, whatever baseline is;
 				% applied to egg duration
-p.dgmaxFry = 0.9; % multiplier on gmaxFW during juvSum1.
+p.dgmaxFry = 0.9; % multiplier on gmaxFW during fry stage (first summer)
 	% Vary this +/- 20% to get 6-8 cm at start of parr
 	% default value is < 1 to tip the base case more clearly into the 18 mo parr scenario
-p.dgmaxParr = 1; % multiplier on gmaxFW during juvLater
+p.dgmaxParr = 1; % multiplier on gmaxFW during parr stage
 	% Vary this about 20% to get 11-15 cm smolts
 % m_smolt is the 4th control knob 
 
@@ -136,7 +140,7 @@ res.m = blank; % mortality
 res.N(1) = p.N_initial;
 res.W(1) = p.W_egg;
 res.L(1) = (p.L3overW .* p.W_egg) .^ (1/3);
-res.t0(1) = datenum('1 Nov 0000') - 366; % Nov before "first year"
+res.t0(1) = p.yearday_eggDeposition;
 
 
 
@@ -145,28 +149,27 @@ for i = 1:nStages-1
 
 	% --- stage duration ---------------------------------
 	
-	dt_i_baseline = p.baselineDuration_months(i) * (365/12);
 	if i==s.egg
 		% temperature-dependent egg duration
-		dt_i = dt_i_baseline ./ p.Q10_dtegg .^ (p.dTwinter./10);
-	elseif i==s.juvSum1
-		% juvSum1 (as opposed to juvLater) is defined as ending 30 Sep in first year
-		dt_i = datenum('30 Sep 0000') - res.t0(s.juvSum1);
-	elseif i==s.juvLater
-		if res.L(s.juvLater) < p.ref_length_parr
-			dt_i = dt_i + 365;
+		dt_i = p.baselineDuration_egg * (365/12) ./ p.Q10_dtegg .^ (p.dTwinter./10);
+	elseif i==s.fry
+		% fry stage is defined as ending 30 Sep in first year
+		dt_i = datenum('30 Sep 0000') - res.t0(s.fry);
+	elseif i==s.parr
+		dt_i = p.baselineDuration_parr * (365/12);
+		if p.flexibleParrDuration
+			if res.L(s.parr) < p.ref_length_parr
+				dt_i = dt_i + 365;
+			end
 		end
-	else
-		% all other stages
-		dt_i = dt_i_baseline; % placeholder
+	else % all other stages
+		dt_i = p.(['baselineDuration_' stages{i}]) * (365/12);
 	end
 
 
 	% --- growth and weight -------------------------------
 	
-	Teff = p.TM_growth; % assume optimal temperature for growth.
-		% Don't show this to the user at this point--it's too ambiguous.
-		% Would the adjustment be around optimal temp or on the rising slope?
+	Teff = p.TM_growth; % assume optimal temperature for growth
 	r_size = res.W(i) .^ -p.exp_growth; % allometry
 	r_temp = (Teff - p.TL_growth) .* ...
 			 (1 - exp(p.gR_growth .* (Teff - p.TU_growth))) ./ ...
@@ -175,13 +178,11 @@ for i = 1:nStages-1
 	r_prey = 1; % haven't included any prey effects
 		% could adjust r_prey for fry based on duration, using the idea that growth is 
 		% concentrated in a 70 day window (Bacon et al. 2005), considering whether 
-		% variation in duration (via dt_egg) falls during that window. Without an 
-		% adjustment for this, will underestimate the effect of small variations in
-		% dt_egg on growth
+		% variation in duration (via dt_egg) falls during that window
 	g = 0;
-	if i == s.juvSum1
+	if i == s.fry
 		g = p.dgmaxFry .* p.gmaxFry .* r_size .* r_temp .* r_prey;
-	elseif i == s.juvLater
+	elseif i == s.parr
 		if dt_i > 365
 			gmax = p.gmaxParr18;
 		else
@@ -201,16 +202,16 @@ for i = 1:nStages-1
 	
 	% density and duration dependence
 	if i == s.egg
-		daily_mort = 1 - (1-p.m_egg)    ^ (1/dt_i_baseline);
+		daily_mort = 1 - (1-p.m_egg)    ^ (1/dt_i);
 		m_i =        1 - (1-daily_mort) ^ (dt_i);
-    elseif i == s.juvSum1
+    elseif i == s.fry
         % apply Ricker density-dependent mortality (scramble competition)
         stock = res.N(i);
         recruits = p.fryRicker * stock * exp((-p.fryRicker/(exp(1)*p.maxParr)) * stock);
         m_i = 1 - (recruits/stock); % total mortality over stage duration
 		% at the moment this is _not_ adjusted for stage duration, even though
 		% fry duration changes in response to egg duration
-    elseif i == s.juvLater
+    elseif i == s.parr
         % apply Beverton-Holt density-dependent mortality
         stock = res.N(i);
         recruits = (p.parrSmoltBH * stock) / (1 + (p.parrSmoltBH/p.maxSmolts) * stock);
@@ -222,7 +223,7 @@ for i = 1:nStages-1
         end
         m_i = 1 - (recruits/stock); % total mortality over stage duration
 	elseif i == s.smolt
-		daily_mort = 1 - (1-p.m_smolt)  ^ (1/dt_i_baseline);
+		daily_mort = 1 - (1-p.m_smolt)  ^ (1/dt_i);
 		m_i =        1 - (1-daily_mort) ^ (dt_i);
 	elseif i >= s.earlyPS && i <= s.adultCoastal
 		refW = p.ref_length_earlyPS^3 / p.L3overW;
